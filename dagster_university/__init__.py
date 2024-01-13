7fba9ca084ff2fa32ac04771d33f184a4b038a74
mg <xynta94@gmail.com>
2024-01-13 16:40:43 +0600

    add schedules to definitio

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,16 +1,18 @@
 # fmt: off
 # dagster_university/__init__.py
-
 from dagster import Definitions, load_assets_from_modules
 
 from .assets import trips, metrics
 from .resources import database_resource
 from .jobs import trip_update_job, weekly_update_job
+from .schedules import trip_update_schedule, weekly_update_schedule
 
 trip_assets = load_assets_from_modules([trips])
 metric_assets = load_assets_from_modules([metrics])
 
 all_jobs = [trip_update_job, weekly_update_job]
+all_schedules = [trip_update_schedule, weekly_update_schedule]
+
 
 defs = Definitions(
   assets=[*trip_assets, *metric_assets],
@@ -18,4 +20,5 @@ defs = Definitions(
       "database": database_resource,
   },
   jobs=all_jobs,
+  schedules=all_schedules,
 )
