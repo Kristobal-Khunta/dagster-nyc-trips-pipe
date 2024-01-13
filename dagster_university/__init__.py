cc310bf66e68adba6471a0761ab182ea4055cb48
mg <xynta94@gmail.com>
2024-01-13 16:40:16 +0600

    add jobs to definitions

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,15 +1,21 @@
 # fmt: off
 # dagster_university/__init__.py
+
 from dagster import Definitions, load_assets_from_modules
+
+from .assets import trips, metrics
 from .resources import database_resource
-from .assets import metrics, trips
+from .jobs import trip_update_job, weekly_update_job
 
 trip_assets = load_assets_from_modules([trips])
 metric_assets = load_assets_from_modules([metrics])
 
+all_jobs = [trip_update_job, weekly_update_job]
+
 defs = Definitions(
-    assets=[*trip_assets, *metric_assets],
-    resources={
-        "database": database_resource,
-    },
+  assets=[*trip_assets, *metric_assets],
+  resources={
+      "database": database_resource,
+  },
+  jobs=all_jobs,
 )
