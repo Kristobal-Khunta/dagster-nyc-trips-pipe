d0fe065a24212d34d475791dcc2286f7c8bf578a
mg <xynta94@gmail.com>
2024-01-15 15:00:09 +0600

    add sensir to generate report

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,12 +1,26 @@
 # jobs/__init__.py
-from dagster import AssetSelection, define_asset_job
+from dagster import AssetSelection, define_asset_job, AssetKey
+
+from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.keys("trips_by_week")
+adhoc_request = AssetSelection.keys(["adhoc_request"])
 
 trip_update_job = define_asset_job(
-    name="trip_update_job", selection=AssetSelection.all() - trips_by_week
+    name="trip_update_job",
+    partitions_def=monthly_partition,
+    selection=AssetSelection.all() - trips_by_week - adhoc_request,
 )
+
+
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
+)
+
+
+adhoc_request_job = define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
 )
